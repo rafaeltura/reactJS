23654ee57144dce7336f4b28239b8298a24cca79
Rafael Ceron Tura <rafaelcerontura@gmail.com>
2022-02-06 10:51:12 -0300

    Atualizado webpack e package.json. Implementado primeiro Componente Funcional React.

--- react-fundamentos/src/index.js
+++ react-fundamentos/src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './App.js';
+
+// ReactDOM.render(
+//     //primeiro elemento ou é uma tag html ou um componente react, ou seja, inicia-se a utilização de JSX
+//     <h1>Olá REACTJS!!!</h1>,
+//     //segundo argumento é o 'container', ou seja, onde será renderizado
+//     document.querySelector('#root')
+// )
+
+/**
+ * Criando um elemento via react, utilizando o conceito Funcional Component
+ */
+
 ReactDOM.render(
-    //primeiro elemento ou é uma tag html ou um componente react, ou seja, inicia-se a utilização de JSX
-    <h1>Olá REACT!</h1>,
-    //segundo argumento é o 'container', ou seja, onde será renderizado
+    <App/>,
     document.querySelector('#root')
 )
--- react-fundamentos/webpack.config.js
+++ react-fundamentos/webpack.config.js
@@ -4,7 +4,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     mode: 'development',
-    entry: path.resolve(__dirname, 'transpiled', 'index.js'),
+    entry: path.resolve(__dirname, 'src', 'index.js'),
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'bundle[hash].js' //hash serve para forçar o navegador a utilizar o arquivo, pois, se não tivesse o hash ele pegaria do cache e, possivelmente, numa versão defasada
@@ -16,5 +16,17 @@ module.exports = {
             }
         ),
         new CleanWebpackPlugin()
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader',
+            }
+        ]
+    },
+    devServer: {
+        port: 3000
+    }
 };
