8ea4d59ce72eaf8035321cbb428b72fa813ffbca
Rafael Ceron Tura <rafaelcerontura@gmail.com>
2022-02-06 16:42:49 -0300

    Trabalhando com Props Default.

--- react-fundamentos/src/Components/subtitles/H2.js
+++ react-fundamentos/src/Components/subtitles/H2.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function H2 (props) {
 
-    return <h2>{ props.titulo || "Subtítulo Componentizado" }</h2>;
+    return <h2>{ props.titulo }</h2>;
 }
 
+H2.propTypes = {
+    titulo: PropTypes.string
+}
+
+H2.defaultProps = {
+    titulo: 'Título H2 default'
+}
 export default H2;
--- react-fundamentos/src/Components/titles/H1.js
+++ react-fundamentos/src/Components/titles/H1.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function H1 ( props ) {
     const { descricao, name } = props.obj;
-    return <h1> { descricao || "Sem Título"} </h1>;
+    return <h1> { descricao } </h1>;
+}
+
+H1.propTypes = {
+    obj: PropTypes.shape({
+        descricao: PropTypes.string,
+        name: PropTypes.string
+    })
+}
+H1.defaultProps = {
+    obj: {
+        descricao: 'Descrição Default',
+        name: 'Name Default'
+    }
 }
 
 export default H1;
