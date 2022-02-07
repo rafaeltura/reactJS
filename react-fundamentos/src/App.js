cb7cd62e873dad2edd1e07bb8ff6c1e029492aa3
Rafael Ceron Tura <rafaelcerontura@gmail.com>
2022-02-06 21:10:19 -0300

    Trabalhando com state, mais especificamente com useState

--- react-fundamentos/src/App.js
+++ react-fundamentos/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import H1 from './Components/titles/H1';
 import H2 from './Components/subtitles/H2';
@@ -26,38 +26,58 @@ const listTarefas = [
     }
 ];
 
-const listPosts = [
-    {
-        id: 1,
-        titulo: "Título Post 1",
-        subtitulo: "Subtítulo Post 1",
-        likes: 20
-    },
-    {
-        id: 2,
-        titulo: "Título Post 2",
-        subtitulo: "Subtítulo Post 2",
-        likes: 22
-    },
-    {
-        id: 3,
-        titulo: "Título Post 3",
-        subtitulo: "Subtítulo Post 3",
-        likes: 11
-    },
-    {
-        id: 4,
-        titulo: "Título Post 4",
-        subtitulo: "",
-        likes: 0
-    },
-];
 /**
  * 
  * PROPS => basicamente servem para passar valores de um componente pai para um componente filho
  *      => São apenas leitura no componente filho, ou seja, se um componente filho tentar sobrescrever alguma props passada do pai ocasionará um erro
  */
 function App() {
+
+    const [ listPosts, setPosts ] = useState([
+        {
+            id: 1,
+            titulo: "Título Post 1",
+            subtitulo: "Subtítulo Post 1",
+            likes: 20
+        },
+        {
+            id: 2,
+            titulo: "Título Post 2",
+            subtitulo: "Subtítulo Post 2",
+            likes: 22
+        },
+        {
+            id: 3,
+            titulo: "Título Post 3",
+            subtitulo: "Subtítulo Post 3",
+            likes: 11
+        },
+        {
+            id: 4,
+            titulo: "Título Post 4",
+            subtitulo: "",
+            likes: 0
+        },
+    ]);
+
+    //console.log({ listPosts });
+
+    function handleRefresh() {
+        setTimeout( () => {
+            setPosts( (prevState) => 
+                [ 
+                    ...prevState,
+                    {
+                        id: (prevState.length + 1),
+                        titulo: `Título Post ${ (prevState.length + 1) }`,
+                        subtitulo: `Sub # ${ (prevState.length + 1) }`,
+                        likes: ( (prevState.length + 1) * 3 )
+                    }
+                ] 
+            );
+        }, 2000);
+    }
+
     return (
         <>
             {/* <H1 obj={ titulo } />
@@ -68,6 +88,7 @@ function App() {
             <Header title="Título Header Posts">
                 <H2 titulo="Subtítulo children componentizado dentro de Header"/>
                 <small>html Children dentro de Header</small>
+                <button onClick={ handleRefresh }> Atualizar Posts </button>
             </Header>
             <hr />
             { 
